60371f44d6f3c30ca8335712bc2124880f4102db
JNCamil <juancamvh@hotmail.com>
2023-06-02 10:15:11 +0200

    sección de comentarios con disqus

--- entrada_individual.php
+++ entrada_individual.php
@@ -27,18 +27,41 @@
                 <?= $entrada_actual["descripcion"] ?>
             </p><br><br>
             <footer class="blockquote-footer">
-                <?= $entrada_actual["fecha"].' | '. $entrada_actual['usuario']?>
+                <?= $entrada_actual["fecha"] . ' | ' . $entrada_actual['usuario'] ?>
             </footer>
 
-        </div>
-    </div>
-<?php if(isset($_SESSION['usuario']) && $_SESSION['usuario']['id'] == $entrada_actual['usuario_id']): ?>
-<button type="button" class="btn btn-warning"><a href="editar_entrada.php?id=<?=$entrada_actual['id']?>" class="text-decoration-none link-light">Editar entrada</a></button>
 
-<button type="button" class="btn btn-danger"><a href="borrar_entrada.php?id=<?=$entrada_actual['id']?>" class="text-decoration-none link-light">Borrar entrada</a></button>
-<?php endif; ?>   
+        </div>
+        
+    </div>
+    <?php if (isset($_SESSION['usuario']) && $_SESSION['usuario']['id'] == $entrada_actual['usuario_id']): ?>
+        <button type="button" class="btn btn-warning"><a href="editar_entrada.php?id=<?= $entrada_actual['id'] ?>"
+                class="text-decoration-none link-light">Editar entrada</a></button>
+
+        <button type="button" class="btn btn-danger"><a href="borrar_entrada.php?id=<?= $entrada_actual['id'] ?>"
+                class="text-decoration-none link-light">Borrar entrada</a></button>
+    <?php endif; ?>
+    <div id="disqus_thread" class="card cartas p-5 m-5"></div>
+        <script>
+            /**
+            *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+            *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+            var disqus_config = function () {
+                this.page.url = '<?php echo "https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]; ?>';
+                this.page.identifier = '<?php echo $entrada_actual["id"]; ?>';
+            };
+            (function () { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://proyecto-blog.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by
+                Disqus.</a></noscript>
 
 </div>
+
 
 <?php require_once("./includes/formularios.php") ?>
 
